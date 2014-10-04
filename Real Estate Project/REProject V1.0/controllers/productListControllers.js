a271a77c4aacd86deb5ffc397ff83aba54851ab6
Jethro <jethro.estrada@yahoo.com>
2014-10-04 02:18:30 -0700

    Real Estate Project prototype 1.1

--- Real Estate Project/REProject V1.0/controllers/productListControllers.js
+++ Real Estate Project/REProject V1.0/controllers/productListControllers.js
@@ -1,8 +1,11 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
-    .constant("productListPageCount", 10)
+    .constant("productListPageCount", 8)
     .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
         var selectedCategory = null;
+        var minimumPrice = 0;
+        var maximumPrice = 0;
+
         $scope.selectedPage = 1;
         $scope.pageSize = productListPageCount;
         $scope.selectCategory = function (newCategory) {
@@ -13,8 +16,14 @@ angular.module("sportsStore")
             $scope.selectedPage = newPage;
         }
         $scope.categoryFilterFn = function (product) {
-            return selectedCategory == null ||
-                product.category == selectedCategory;
+            if(selectedCategory == null && minimumPrice == 0 && maximumPrice == 0){
+                return true;
+            }
+            var isLocated = selectedCategory ? (product.category == selectedCategory) : true;
+            var isGreaterMinPrice = minimumPrice ? (product.price >= minimumPrice) : true
+            var isLesserMaxPrice = maximumPrice ? (product.price <= maximumPrice) : true;
+
+            return (isLocated && isGreaterMinPrice && isLesserMaxPrice);
         }
         $scope.getCategoryClass = function (category) {
             return selectedCategory == category ? productListActiveClass : "";
@@ -24,5 +33,13 @@ angular.module("sportsStore")
         }
         $scope.addProductToCart = function (product) {
             cart.addProduct(product.id, product.name, product.price);
+        }
+
+        $scope.searchProduct = function(location, minPrice, maxPrice){
+            selectedCategory = location ? location : null;
+            minimumPrice = minPrice ? minPrice : 0;
+            maximumPrice = maxPrice ? maxPrice : 0;
+
+            $scope.selectedPage = 1;
         }
     });

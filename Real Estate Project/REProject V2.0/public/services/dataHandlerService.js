6c8ae2ae373c4391f205d9f53537a47fc19da314
Jethro <jethro.estrada@yahoo.com>
2014-10-27 02:16:45 -0700

    remove unneeded files

--- Real Estate Project/REProject V2.0/public/services/dataHandlerService.js
+++
@@ -1,9 +0,0 @@
-angular.module("sportsStore")
-    .factory("dataHandler",function(items){
-       var savedItems = items;
-        return {
-            getItems : function(){
-                return savedItems;
-            }
-        }
-    });
